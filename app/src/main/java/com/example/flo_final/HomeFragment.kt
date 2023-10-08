10b7d92995bf4ba989241c49ea3e94d5d5517906
chahyeonjeong <oliveyou12@naver.com>
2023-10-09 02:37:27 +0900

    initial commit

---
+++ app/src/main/java/com/example/flo_final/HomeFragment.kt
@@ -0,0 +1,33 @@
+package com.example.flo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.flo_final.AlbumFragment
+import com.example.flo_final.MainActivity
+import com.example.flo_final.R
+import com.example.flo_final.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    lateinit var binding: FragmentHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.homePannelAlbumTodayImg1.setOnClickListener {
+            (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm,AlbumFragment()).commitAllowingStateLoss()
+        }
+        return binding.root
+    }
+}
