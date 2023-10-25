1183dc71131a9aba8940177d330b72c591890a1b
jeong724 <oliveyou724@gmail.com>
2023-10-26 00:22:19 +0900

    week4_seekbar,splash

--- app/src/main/java/com/example/flo_final/Song.kt
+++ app/src/main/java/com/example/flo_final/Song.kt
@@ -4,5 +4,8 @@ import android.icu.text.CaseMap.Title
 
 data class Song(
     val title : String = "",
-    val singer : String = ""
+    val singer : String = "",
+    var second : Int = 0,
+    var playTime : Int = 0,
+    var isPlaying : Boolean = false
 )
