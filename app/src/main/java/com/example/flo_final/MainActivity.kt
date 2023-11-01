44112aa0b46cfe57cf638eaef079ba88d7bee398
jeong724 <oliveyou724@gmail.com>
2023-11-02 07:38:16 +0900

    mission5_mainseekbar

--- app/src/main/java/com/example/flo_final/MainActivity.kt
+++ app/src/main/java/com/example/flo_final/MainActivity.kt
@@ -6,18 +6,20 @@ import com.example.flo.HomeFragment
 import com.example.flo_final.databinding.ActivityMainBinding
 import android.content.Intent
 import android.util.Log
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+
+    private var song:Song = Song()
+    private var gson : Gson = Gson()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_FLO)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val song = Song(binding.mainMiniplayerTitleTv.text.toString(), binding.mainMiniplayerSingerTv.text.toString(), 0, 60, false, "music_lilac")
 
         binding.mainPlayerCl.setOnClickListener{
             //startActivity(Intent(this, SongActivity::class.java))
@@ -72,5 +74,27 @@ class MainActivity : AppCompatActivity() {
             }
             false
         }
+    }
+
+    private fun setMiniPlayer(song: Song){
+        binding.mainMiniplayerSingerTv.text=song.singer
+        binding.mainMiniplayerTitleTv.text=song.title
+        binding.mainMiniplayerProgressSb.progress = (song.second*100000) / song.playTime
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        val sharedPreferences = getSharedPreferences("song", MODE_PRIVATE)
+        val songJson = sharedPreferences.getString("songData", null)
+
+        song = if (songJson == null){
+            Song("라일락", "아이유", 0, 60, false, "music_lilac")
+        }
+        else{
+            gson.fromJson(songJson, Song::class.java)
+        }
+
+        setMiniPlayer(song)
     }
 }
