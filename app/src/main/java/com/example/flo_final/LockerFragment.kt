6b5516c1054195299a2e3f55685810ff799a3f65
jeong724 <oliveyou724@gmail.com>
2023-12-01 16:49:12 +0900

    week7_locker_error

--- app/src/main/java/com/example/flo_final/LockerFragment.kt
+++ app/src/main/java/com/example/flo_final/LockerFragment.kt
@@ -5,33 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.flo_final.LockerVPAdapter
 import com.example.flo_final.databinding.FragmentLockerBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
-
 class LockerFragment : Fragment() {
-    lateinit var binding : FragmentLockerBinding
-
-    private val information= arrayListOf("저장한 곡", "음악파일")
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    lateinit var binding: FragmentLockerBinding
+    private val information = arrayListOf("저장한곡", "음악파일")
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding=FragmentLockerBinding.inflate(inflater, container, false)
+    ): View {
+        binding = FragmentLockerBinding.inflate(inflater, container, false)
 
-        val lockerAdapter=LockerVPAdapter(this)
-        binding.lockerVp.adapter=lockerAdapter
-        TabLayoutMediator(binding.lockerTab,binding.lockerVp){
-            tab, position -> tab.text=information[position]
+        val lockerAdapter = LockerVPAdapter(this)
+        binding.lockerContentVp.adapter = lockerAdapter
+        TabLayoutMediator(binding.lockerContentTb, binding.lockerContentVp) { tab, position ->
+            tab.text = information[position]
         }.attach()
 
         return binding.root
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_locker, container, false)
     }
 }
